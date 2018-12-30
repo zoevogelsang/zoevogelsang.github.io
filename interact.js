31422672b7d48eb7ef9e7c8c379a8d0c71b2036e
zoevogelsang <40878170+zoevogelsang@users.noreply.github.com>
2018-12-30 05:01:49 -0500

    Create interact.js

---
+++ interact.js
@@ -0,0 +1,19 @@
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
